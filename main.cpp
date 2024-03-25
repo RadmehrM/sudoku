1eea439a4aaeb897ffe87b4c8d587acbf8ead258
Radmehr <radmehrmehdipour1234@gmail.com>
2024-03-25 16:55:17 -0400

    fusion styles

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "window.h"
 #include <QApplication>
+#include<QStyleFactory>
  
 int main(int argc, char *argv[]) 
 {
   QApplication app(argc, argv);
+  QApplication::setStyle(QStyleFactory::create("Fusion"));
   Window window;
   window.resize(800,800);
   window.setWindowTitle("Sudoku");
